gitter-badger/deployer-2

Only enable the deploy button if all servers have been successfully tested

resources/views/projects/details.blade.php
@@ -132,6 +132,6 @@
 @section('right-buttons')
     <div class="pull-right">
         <button type="button" class="btn btn-default" title="{{ Lang::get('projects.view_ssh_key') }}" data-toggle="modal" data-target="#key"><span class="fa fa-key"></span> {{ Lang::get('projects.ssh_key') }}</button>
-        <button id="deploy_project" data-toggle="modal" data-backdrop="static" data-target="#reason" type="button" class="btn btn-danger" title="{{ Lang::get('projects.deploy_project') }}" {{ ($project->isDeploying() OR !count($project->servers)) ? 'disabled' : '' }}><span class="fa fa-cloud-upload"></i> {{ Lang::get('projects.deploy') }}</button>
+        <button id="deploy_project" data-toggle="modal" data-backdrop="static" data-target="#reason" type="button" class="btn btn-danger" title="{{ Lang::get('projects.deploy_project') }}" {{ ($project->isDeploying() OR !$project->canDeploy()) ? 'disabled' : '' }}><span class="fa fa-cloud-upload"></i> {{ Lang::get('projects.deploy') }}</button>
     </div>
 @stop

jcornide/deployer

resources/views/projects/details.blade.php
@@ -130,6 +130,6 @@
 @section('right-buttons')
     <div class="pull-right">
         <button type="button" class="btn btn-default" title="{{ Lang::get('projects.view_ssh_key') }}" data-toggle="modal" data-target="#key"><span class="fa fa-key"></span> {{ Lang::get('projects.ssh_key') }}</button>
-        <button id="deploy_project" data-toggle="modal" data-backdrop="static" data-target="#reason" type="button" class="btn btn-danger" title="{{ Lang::get('projects.deploy_project') }}" {{ ($project->isDeploying() OR !count($project->servers)) ? 'disabled' : '' }}><span class="fa fa-cloud-upload"></span> {{ Lang::get('projects.deploy') }}</button>
+        <button id="deploy_project" data-toggle="modal" data-backdrop="static" data-target="#reason" type="button" class="btn btn-danger" title="{{ Lang::get('projects.deploy_project') }}" {{ ($project->isDeploying() OR !$project->canDeploy()) ? 'disabled' : '' }}><span class="fa fa-cloud-upload"></span> {{ Lang::get('projects.deploy') }}</button>
     </div>
 @stop